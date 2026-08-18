edbd824afaed6383409f9d607fa8fcce11119467
rspoelstra216 <randal.spoelstra@gmail.com>
2026-08-18 07:01:01 -0400

    Load featured ministry support

--- surfside-tools.php
+++ surfside-tools.php
@@ -39,6 +39,7 @@ require_once SURFSIDE_TOOLS_PATH . 'includes/site-settings-hub.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/calendar-manager.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/bible-studies.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/event-audiences.php';
+require_once SURFSIDE_TOOLS_PATH . 'includes/featured-ministries.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/ministry-events.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/calendar-day-details.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/calendar-simple-overflow-layout.php';
