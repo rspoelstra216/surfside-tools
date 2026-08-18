47783d1afd6d414ac3471663e4d2513ac2d26d6f
rspoelstra216 <randal.spoelstra@gmail.com>
2026-08-18 09:13:29 -0400

    Load dynamic Serve ministry section

--- surfside-tools.php
+++ surfside-tools.php
@@ -42,6 +42,7 @@ require_once SURFSIDE_TOOLS_PATH . 'includes/event-audiences.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/featured-ministries.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/ministry-events.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/ministry-manager-featured.php';
+require_once SURFSIDE_TOOLS_PATH . 'includes/ministry-serve-section.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/calendar-day-details.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/calendar-simple-overflow-layout.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/calendar-month-navigation.php';
