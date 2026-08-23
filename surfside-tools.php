ecec665533908fe8fd07bf84fa310f6689453502
rspoelstra216 <randal.spoelstra@gmail.com>
2026-08-23 09:32:47 -0400

    Load ministry publishing workflow

--- surfside-tools.php
+++ surfside-tools.php
@@ -37,6 +37,7 @@ require_once SURFSIDE_TOOLS_PATH . 'includes/site-information-manager.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/ministries-manager.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/ministry-featured-manager.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/ministry-ui-polish.php';
+require_once SURFSIDE_TOOLS_PATH . 'includes/ministry-publishing.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/site-management-hub.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/site-settings-hub.php';
 require_once SURFSIDE_TOOLS_PATH . 'includes/calendar-manager.php';
